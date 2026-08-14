908b11e798773de71d29f28791c205ce9d15e18e
daniaHasan107 <23-101147@students.eui.edu.eg>
2026-08-14 19:03:21 +0300

    Add application logic for code coverage

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,4 +10,23 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	public static double calculateBalance(double deposit, double withdrawal) {
+		return deposit - withdrawal;
+	}
+
+	public static boolean isEligibleForLoan(double salary, double creditScore) {
+		return salary >= 10000 && creditScore >= 650;
+	}
+
+	public static String getAccountStatus(double balance) {
+		if (balance > 0) {
+			return "ACTIVE";
+		}
+
+		return "EMPTY";
+	}
+
+	public static double calculateInterest(double amount, double rate) {
+		return amount * rate / 100;
+	}
 }
