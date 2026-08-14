8186346975b42ce8e8c1bbfa7075cfb0d5b51d2e
daniaHasan107 <23-101147@students.eui.edu.eg>
2026-08-14 19:07:31 +0300

    Improve application tests and code coverage

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class DemoApplicationTests {
 
@@ -10,4 +12,47 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void shouldCalculateBalance() {
+		assertEquals(
+				7000,
+				DemoApplication.calculateBalance(10000, 3000)
+		);
+	}
+
+	@Test
+	void shouldCheckLoanEligibility() {
+		assertTrue(
+				DemoApplication.isEligibleForLoan(15000, 700)
+		);
+
+		assertFalse(
+				DemoApplication.isEligibleForLoan(8000, 700)
+		);
+
+		assertFalse(
+				DemoApplication.isEligibleForLoan(15000, 600)
+		);
+	}
+
+	@Test
+	void shouldReturnAccountStatus() {
+		assertEquals(
+				"ACTIVE",
+				DemoApplication.getAccountStatus(5000)
+		);
+
+		assertEquals(
+				"EMPTY",
+				DemoApplication.getAccountStatus(0)
+		);
+	}
+
+	@Test
+	void shouldCalculateInterest() {
+		assertEquals(
+				500,
+				DemoApplication.calculateInterest(10000, 5)
+		);
+	}
 }
